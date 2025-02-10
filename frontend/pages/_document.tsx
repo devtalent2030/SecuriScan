db5aad9a7d1b76bcb75b713791704b5ee339d017
AZander48 <Alexzander4668@gmail.com>
2025-02-10 15:44:28 -0500

    theme added

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,13 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Header from './utils/header'
+import Footer from './utils/footer'
+import ThemeProvider from "./utils/ThemeProvider";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
+		<Head />
+		<ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        >
+			<Header />
+			<body className="antialiased">
+				<Main />
+				<NextScript />
+			</body>
+			<Footer />
+      	</ThemeProvider>
     </Html>
   );
 }
