20c85988351fccce84edeb67f7b68128a6d21bc6
AZander48 <Alexzander4668@gmail.com>
2025-03-05 13:55:59 -0500

    update page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 							</li>
 							<li>
 								<div className="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
-									<input id="checkbox-item-crawler" type="checkbox" 
+									<input id="checkbox-item-SQLInjection" type="checkbox" 
 									className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
 									<label htmlFor="checkbox-item-SQLInjection" 
 									className="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm dark:text-gray-300">
